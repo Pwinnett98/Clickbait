2608377777aa1e5b0e9ca9432fef58887b74c524
Paige Winnett <winnettpaige2@gmail.com>
2024-10-23 14:36:51 -0500

    save 4

--- app/src/main/java/com/example/clickbait/MainActivity.kt
+++ app/src/main/java/com/example/clickbait/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,10 +12,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -73,23 +77,20 @@ ad: Ad,
 modifier: Modifier = Modifier
 ) {
     Card(modifier = modifier) {
-        Row(
+        Column(
             modifier = modifier
                 .fillMaxWidth()
                 .padding(8.dp)
         ) {
             Image(
                 modifier = modifier
-                    .size(64.dp)
-                    .padding(8.dp)
+                    .size(width = 150.dp, height = 150.dp)
+                    .padding(0.dp)
                     .clip(MaterialTheme.shapes.medium),
                 contentScale = ContentScale.FillWidth,
                 painter = painterResource(ad.imageResourceId),
                 contentDescription = null
             )
-
-
-            Column {
                 Text(
                     text = stringResource(ad.creator),
                     style = MaterialTheme.typography.displayLarge,
@@ -101,7 +102,7 @@ modifier: Modifier = Modifier
                         modifier = Modifier.padding(top = 8.dp) ,
                         maxLines = 4, overflow = TextOverflow.Ellipsis
                     )
-                Divider()
+                HorizontalDivider()
                 Text(
                     text = stringResource(ad.time),
                     style = MaterialTheme.typography.displaySmall,
@@ -110,7 +111,6 @@ modifier: Modifier = Modifier
             }
         }
     }
-}
 
 
 @Composable
@@ -127,6 +127,16 @@ fun ClickbaitApp() {
                     modifier = Modifier.padding(8.dp)
                 )
             }
+        }
+    }
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.padding(8.dp)
+    ) {
+        items(DataSource.Ads) { topic ->
+            AdItem(topic)
         }
     }
 }
